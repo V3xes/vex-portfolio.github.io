65cdb22058f9870253e159f3be0ff1b8520f6c3c
V3xes <168699239+V3xes@users.noreply.github.com>
2025-06-30 03:12:59 -0600

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('nav button').forEach(button => {
-  button.addEventListener('click', () => {
-    const targetId = button.getAttribute('data-target');
-    const target = document.getElementById(targetId);
-    if (target) {
-      target.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-});
